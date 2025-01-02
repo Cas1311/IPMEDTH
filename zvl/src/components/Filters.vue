fd6c2617382d019966ec9308b2dd95d78662b77b
hnvz <s1127812@student.hsleiden.nl>
2025-01-02 21:48:19 +0100

    updated css a bit for the filter panel

--- zvl/src/components/Filters.vue
+++ zvl/src/components/Filters.vue
@@ -1,52 +1,53 @@
 <template>
-    <Panel header="Filters" toggleable collapsed>
+    <div class="filter-container">
+        <Panel class="filter-panel" header="Filters" toggleable collapsed>
 
-        <div class="slidercontainer">
-            <label>Selecteer categorie and onderdelen</label>
-            <multiselect v-model="skillValue" :options="options" :multiple="true" group-values="skill" group-label="category"
-                :group-select="true" placeholder="Typ om te zoeken" track-by="name" label="name"><span
-                    slot="noResult">Geen resultaten gevonden voor je zoekopdracht</span>
-            </multiselect>
-        </div>
+            <div class="filter-item-container">
+                <h3>Selecteer categorie of onderdelen</h3>
+                <multiselect v-model="skillValue" :options="options" :multiple="true" group-values="skill"
+                    group-label="category" :group-select="true" placeholder="Typ om te zoeken" track-by="name"
+                    label="name"><span slot="noResult">Geen resultaten gevonden voor je zoekopdracht</span>
+                </multiselect>
+            </div>
 
-        <div class="slidercontainer">
-            <h3>Vanaf leeftijd</h3>
-            <select v-model.number="minimumAge">
-                <option value="" disabled >Selecteer leeftijd</option>
-                <option value="8">8</option>
-                <option value="10">10</option>
-                <option value="12">12</option>
-                <option value="14">14</option>
-                <option value="16">16</option>
-                <option value="18">18</option>
-            </select>
-        </div>
+            <div class="filter-item-container">
+                <h3>Vanaf leeftijd</h3>
+                <select v-model.number="minimumAge">
+                    <option value="" disabled>Selecteer leeftijd</option>
+                    <option value="8">8</option>
+                    <option value="10">10</option>
+                    <option value="12">12</option>
+                    <option value="14">14</option>
+                    <option value="16">16</option>
+                    <option value="18">18</option>
+                </select>
+            </div>
 
-        <div class="slidercontainer">
-            <h3>Type oefening</h3>
-            <select v-model.number="waterValue">
-                <option value="">Alles</option>
-                <option value="1">Water</option>
-                <option value="0">Niet water</option>
-            </select>
-        </div>
+            <div class="filter-item-container">
+                <h3>Type oefening</h3>
+                <select v-model.number="waterValue">
+                    <option value="">Alles</option>
+                    <option value="1">Water</option>
+                    <option value="0">Niet water</option>
+                </select>
+            </div>
 
-        <div class="slidercontainer">
-            <h3>Minimum aantal spelers</h3>
-            <InputText class="durationinput" v-model.number="playerSliderValue" />
-            <Slider class="slider" v-model="playerSliderValue" :min="1" :max="10" />
-        </div>
+            <div class="filter-item-container">
+                <h3>Minimum aantal spelers</h3>
+                <InputText class="durationinput" v-model.number="playerSliderValue" />
+                <Slider class="slider" v-model="playerSliderValue" :min="1" :max="10" />
+            </div>
 
 
-        <div class="slidercontainer">
-            <h3>Benodigde tijd</h3>
-            <InputText class="durationinput" v-model.number="durationSliderValue[0]" />
-            <InputText class="durationinput" v-model.number="durationSliderValue[1]" />
-            <Slider class="slider" v-model="durationSliderValue" range :min="1" :max="60" />
-        </div>
+            <div class="filter-item-container">
+                <h3>Benodigde tijd</h3>
+                <InputText class="durationinput" v-model.number="durationSliderValue[0]" />
+                <InputText class="durationinput" v-model.number="durationSliderValue[1]" />
+                <Slider class="slider" v-model="durationSliderValue" range :min="1" :max="60" />
+            </div>
 
-    </Panel>
-
+        </Panel>
+    </div>
 </template>
 
 <script>
@@ -82,25 +83,25 @@ export default {
             handler(newValue) {
                 this.$emit('duration-slider-changed', newValue);
             },
-            deep: true 
+            deep: true
         },
         playerSliderValue: {
             handler(newValue) {
                 this.$emit('player-slider-changed', newValue);
             },
-            deep: true 
+            deep: true
         },
         minimumAge: {
             handler(newValue) {
                 this.$emit('age-changed', newValue);
             },
-            deep: true 
+            deep: true
         },
         waterValue: {
             handler(newValue) {
                 this.$emit('water-changed', newValue);
             },
-            deep: true 
+            deep: true
         }
     },
 
@@ -148,17 +149,29 @@ export default {
 </script>
 <style src="vue-multiselect/dist/vue-multiselect.css"></style>\
 <style scoped>
-.slidercontainer {
+.filter-container {
+    
+}
+
+.filter-panel {
+
+    margin: 1em;
+}
+
+.p-panel {
+    background-color: var(--theme-primary);
+    border-color: var(--theme-secondary);
+}
+
+.filter-item-container {
     padding: 1em;
-
-
 }
 
 .durationinput {
     margin: 0.5em;
 }
 
-.slider{
+.slider {
     max-width: 80%;
 }
 </style>
